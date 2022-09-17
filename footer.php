41573d2b6ffb1f0f5193b89e06857e9ab4a929da
tosshii <>
2022-09-17 19:10:40 +0900

    no message

--- footer.php
+++ footer.php
@@ -4,24 +4,27 @@
  * フッターテンプレート
  */
 ?>
-<footer class="ly_footer">
-	<div class="ly_footer_inner">
-		<nav class="bl_footerNav" aria-label="フッターナビゲーションメニュー">
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'footer',
-					'container' => false,
-					'menu_class' => 'bl_footerList',
-					'menu_id' => 'bl_footerList',
-					'fallback_cb' => false,
-				)
-			);
-			?>
-		</nav>
-		<small class="bl_footerCopy">&copy;2021 - <?php echo date('Y'); ?></small>
+	<footer class="ly_footer">
+		<div class="ly_footer_inner">
+			<nav class="bl_footerNav" aria-label="フッターナビゲーションメニュー">
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'footer',
+						'container' => false,
+						'menu_class' => 'bl_footerList',
+						'menu_id' => 'bl_footerList',
+						'fallback_cb' => false,
+					)
+				);
+				?>
+			</nav>
+			<small class="bl_footerCopy">&copy;2021 - <?php echo date('Y'); ?></small>
+		</div>
+	</footer>
+	<div class="el_scrollTop js_scrollTop">
+		<button type="button" aria-label="ページトップボタン"></button>
 	</div>
-</footer>
 </div>
 <?php wp_footer(); ?>
 </body>
